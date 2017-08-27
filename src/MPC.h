8da46ffd547639390e1ceb5f00afcad390e1f2ec
vladimirstanovov <vladimirstanovov@yandex.ru>
2017-08-27 14:38:20 +0700

    initial commit

---
+++ src/MPC.h
@@ -0,0 +1,32 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include <cppad/cppad.hpp>
+#include <cppad/ipopt/solve.hpp>
+#include "Eigen-3.3/Eigen/Core"
+
+using namespace std;
+
+using CppAD::AD;
+
+typedef CPPAD_TESTVECTOR(double) Dvector;
+
+class MPC {
+ public:
+  MPC();
+
+  virtual ~MPC();
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  //CppAD::ipopt::solve_result<Dvector> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+};
+
+struct Solution
+{
+
+};
+
+#endif /* MPC_H */
